7963dc8f66d4d9134637825f56bc847b460a7f6c
iSnortPB <ev_padgett@hotmail.com>
2024-09-18 23:27:51 -0500

    Add files via upload

---
+++ win.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+// cd C:/Users/Ev_Pa/Downloads/SE1850
+
+int main() 
+{
+    char win[50] = " ";
+
+    printf("do you win?\n");
+    scanf("%s", &win[50]);
+    printf("ok\n");
+
+    return 0;
+}
